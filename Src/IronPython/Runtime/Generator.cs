5be0290077147b50e08207c4001488ff3655028f
Alex Earl <slide.o.mix@gmail.com>
2017-06-21 16:18:48 -0700

    Fix #10 (#173)
    * Fix #10
    
    Update exception message
    
    * Update code for generators to output the correct message
    
    * Rework methods

--- Src/IronPython/Runtime/Generator.cs
+++ Src/IronPython/Runtime/Generator.cs
@@ -134,7 +134,7 @@ namespace IronPython.Runtime {
             // - not update any other generator state (so future calls to Next() will still work)
             if (type == null) {
                 // Create the appropriate exception and throw it.
-                throw PythonOps.MakeExceptionTypeError(null);
+                throw PythonOps.MakeExceptionTypeError(null, true);
             }
 
             // Set fields which will then be used by CheckThrowable.
@@ -527,7 +527,7 @@ namespace IronPython.Runtime {
 
             // This may invoke user code such as __init__, thus MakeException may throw. 
             // Since this is invoked from the generator's body, the generator can catch this exception. 
-            return LightExceptions.Throw(PythonOps.MakeException(Context, throwableBackup[0], throwableBackup[1], throwableBackup[2]));
+            return LightExceptions.Throw(PythonOps.MakeExceptionForGenerator(Context, throwableBackup[0], throwableBackup[1], throwableBackup[2]));
         }
 
         private void Close() {
