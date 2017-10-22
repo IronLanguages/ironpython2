b4f9c222c3a859150077ea7aeb3eaeb04e4ce9c9
Alex Earl <slide.o.mix@gmail.com>
2017-10-22 07:52:38 -0700

    Remove redirection of test, dir command and change to make.cmd before testing

--- make.cmd
+++ make.cmd
@@ -139,7 +139,6 @@ for %%f in ("%FRAMEWORKS:,=" "%") do (
 :test-all
 for %%f in ("%FRAMEWORKS:,=" "%") do (
   pushd bin\Release\%%f
-  dir
   ..\..\..\packages\nunit.consolerunner\3.6.1\tools\nunit3-console.exe  --labels=All --result:all-%%f-release-result.xml IronPythonTest.dll
   popd
 )
