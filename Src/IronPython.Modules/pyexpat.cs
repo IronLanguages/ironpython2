1db55dda0cd79a1a1f221c2649a96059ba165295
Alex Earl <slide.o.mix@gmail.com>
2018-07-14 13:36:11 -0700

    Add license header

--- Src/IronPython.Modules/pyexpat.cs
+++ Src/IronPython.Modules/pyexpat.cs
@@ -1,4 +1,8 @@
-﻿using IronPython.Runtime;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using IronPython.Runtime;
 using IronPython.Runtime.Exceptions;
 using IronPython.Runtime.Operations;
 using IronPython.Runtime.Types;
