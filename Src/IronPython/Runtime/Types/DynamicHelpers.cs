efb557cb117837a823717c9df5f1585e2d824a96
slozier <slozier@users.noreply.github.com>
2018-07-31 22:08:59 -0400

    Path fix for loading compiled modules cross-platorm (#478)

--- Src/IronPython/Runtime/Types/DynamicHelpers.cs
+++ Src/IronPython/Runtime/Types/DynamicHelpers.cs
@@ -19,8 +19,7 @@ namespace IronPython.Runtime.Types {
         }
 
         public static PythonType GetPythonType(object o) {
-            IPythonObject dt = o as IPythonObject;
-            if (dt != null) return dt.PythonType;
+            if (o is IPythonObject dt) return dt.PythonType;
             
             return GetPythonTypeFromType(CompilerHelpers.GetType(o));
         }
