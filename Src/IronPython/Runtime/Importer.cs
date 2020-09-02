0e2a33e78935ba5cfa12d4b525b47de1a0e38948
Stéphane Lozier <slozier@users.noreply.github.com>
2020-09-02 14:44:05 -0400

    Don't insert __file__ to sys.path

--- Src/IronPython/Runtime/Importer.cs
+++ Src/IronPython/Runtime/Importer.cs
@@ -857,15 +857,14 @@ namespace IronPython.Runtime {
                 return false;
             }
             importCache[path] = importer = FindImporterForPath(context, path);
-            if (importer == null) {
+            if (importer is null || importer is PythonImport.NullImporter) {
                 return false;
             }
             // for consistency with cpython, insert zip as a first entry into sys.path
             if (context.LanguageContext.GetSystemStateValue("path") is List syspath) {
                 syspath.Insert(0, path);
             }
-            object dummy;
-            return FindAndLoadModuleFromImporter(context, importer, "__main__", null, out dummy);
+            return FindAndLoadModuleFromImporter(context, importer, "__main__", null, out _);
         }
 
         private static object LoadFromDisk(CodeContext context, string name, string fullName, string str) {
